4ed4dc4990a493f39378bac60b8bc774fb818ff1
Artem <ppt.artem@gmail.com>
2022-09-08 11:13:45 +0300

    dbgeters_idtoname

--- src/js/api/moviedb/getMoviesDetails.js
+++ src/js/api/moviedb/getMoviesDetails.js
@@ -1,1 +1,15 @@
-import './getGenres'
+import { API_KEY, BASE_URL } from '../../constants/moviedb';
+import axios from 'axios';
+
+const END_POINT = 'movie/';
+export async function getTrending(id) {
+  const response = await axios.get(BASE_URL + END_POINT + id, {
+    params: {
+      api_key: API_KEY,
+    },
+  });
+
+  return response.data;
+}
+
+// getTrending(84773).then(data => console.log(data));
