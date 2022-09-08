0c20cff77b65108b693e6f281bdfc504ef641d09
Artem <ppt.artem@gmail.com>
2022-09-08 17:30:34 +0300

    fix_geters_name

--- src/js/api/moviedb/getMoviesDetails.js
+++ src/js/api/moviedb/getMoviesDetails.js
@@ -2,7 +2,7 @@ import { API_KEY, BASE_URL } from '../../constants/moviedb';
 import axios from 'axios';
 
 const END_POINT = 'movie/';
-export async function getTrending(id) {
+export async function getMoviesDetails(id) {
   const response = await axios.get(BASE_URL + END_POINT + id, {
     params: {
       api_key: API_KEY,
@@ -12,4 +12,4 @@ export async function getTrending(id) {
   return response.data;
 }
 
-// getTrending(84773).then(data => console.log(data));
+// getMoviesDetails(84773).then(data => console.log(data));
