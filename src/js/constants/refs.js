8258b90ad14eda357e42f8f6b8416acd241df625
Olena Posternak <alena.kavyrshyna@gmail.com>
2022-09-09 13:33:17 +0300

    loader html css js

--- src/js/constants/refs.js
+++ src/js/constants/refs.js
@@ -3,5 +3,6 @@ export const refs = {
   headerForm: document.querySelector(".header__form"),
   footerBtn: document.querySelector(".footer__btn"),
   headerWatchedBtn: document.querySelector(".js-watched > button"),
-  headerQueueBtn: document.querySelector(".js-queue > button")
+  headerQueueBtn: document.querySelector(".js-queue > button"),
+  loader: document.querySelector(`.js-page-loader`),
 }
