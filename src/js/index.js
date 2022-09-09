6ec61bad0961505d49abaac4285d955073c1b2be
syniepolskyi <syniepolskyi@yahoo.com>
2022-09-09 14:59:28 +0300

    quick fix index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,6 @@
 import './constants/refs';
 import './api/moviedb/getTrending';
 import './api/moviedb/searchMovies';
-import './queue';
-import './watched';
 import './modalCard';
 import './modalAbout';
 import './upToTop';
