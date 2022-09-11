4f34da52ffe34addf203a762fd3c035c9dfcb1e5
Olena Posternak <alena.kavyrshyna@gmail.com>
2022-09-11 18:52:12 +0300

    +

--- src/js/features/theme-switcher/theme-switcher.js
+++ src/js/features/theme-switcher/theme-switcher.js
@@ -1,1 +1,24 @@
+const themeSwither = document.querySelectorAll(`.change_theme`);
+
+const changeThemeCssLink = document.querySelector(`[title= "theme"]`);
+
+
+themeSwither.forEach(swither =>
+  swither.addEventListener(`click`, function () {
+
+    if (this.dataset.theme === `onDark`) {
+      console.log(this.dataset.theme);
+      console.log(changeThemeCssLink.disabled);
+      changeThemeCssLink.disabled = false;
+      
+
+    }
+    if (this.dataset.theme === `onLight`) {
+      console.log(this.dataset.theme);
+      console.log(changeThemeCssLink.disabled);
+      changeThemeCssLink.disabled = true;
+
+    }
+  })
+);
 
