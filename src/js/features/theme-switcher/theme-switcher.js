8980d7927a34f897ef71910ea1391db283dba275
Olena Posternak <alena.kavyrshyna@gmail.com>
2022-09-11 23:57:55 +0300

    +

--- src/js/features/theme-switcher/theme-switcher.js
+++ src/js/features/theme-switcher/theme-switcher.js
@@ -1,24 +1,24 @@
 const themeSwither = document.querySelectorAll(`.change_theme`);
-
 const changeThemeCssLink = document.querySelector(`[title= "theme"]`);
 
+themeSwither.forEach(swither => swither.addEventListener(`click`, changeTheme));
 
-themeSwither.forEach(swither =>
-  swither.addEventListener(`click`, function () {
+function changeTheme() {
+  localStorage.setItem(`theme`, this.dataset.theme);
 
-    if (this.dataset.theme === `onDark`) {
-      console.log(this.dataset.theme);
-      console.log(changeThemeCssLink.disabled);
-      changeThemeCssLink.disabled = false;
-      
+  if (this.dataset.theme === `onDark`) {
+    changeThemeCssLink.disabled = false;
+  }
+  if (this.dataset.theme === `onLight`) {
+    changeThemeCssLink.disabled = true;
+  }
+}
 
-    }
-    if (this.dataset.theme === `onLight`) {
-      console.log(this.dataset.theme);
-      console.log(changeThemeCssLink.disabled);
-      changeThemeCssLink.disabled = true;
+let activeTheme = localStorage.getItem(`theme`);
 
-    }
-  })
-);
-
+if (activeTheme === null || activeTheme === `onLight`) {
+  changeThemeCssLink.disabled = true;
+} 
+else {
+  changeThemeCssLink.disabled = false;
+}
