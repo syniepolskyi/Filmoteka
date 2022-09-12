d92d2e50675ab166b086e0da1d3bbb530e23c781
Nazar Lyubuska <nazar.lyubuska@gmail.com>
2022-09-12 12:32:00 +0300

    message

--- src/js/modalAbout.js
+++ src/js/modalAbout.js
@@ -1,141 +1,66 @@
 import Swiper, { Navigation, Pagination } from 'swiper';
-const bodyEl = document.querySelector('body');
-const modalTeam = document.querySelector('.js-about-modal');
-const btnCloseModalTeam = document.querySelector('.js-close-about-modal');
-const btnOpenModalTeam = document.querySelector('.js-open-modal');
+const aboutModal = document.querySelector('.js-about-modal');
+const body = document.querySelector('body');
+const btnOpenAboutModal = document.querySelector('.js-open-modal');
+const btnCloseAboutModal = document.querySelector('.js-close-about-modal');
 
-btnOpenModalTeam.addEventListener('click', openModalTeam);
-btnCloseModalTeam.addEventListener('click', closeModalTeam);
+btnOpenAboutModal.addEventListener('click', openAboutModal);
+btnCloseAboutModal.addEventListener('click', closeAboutModal);
 
-function openModalTeam() {
-    modalTeam.classList.remove('is-hidden')
-    bodyEl.classList.add('modal-open');
+function openAboutModal() {
+    aboutModal.classList.remove('is-hidden')
+    body.classList.add('modal-open');
 
-    document.addEventListener('keydown', onKeyboardPush);
-    document.addEventListener('click', cliskIntoVoid);
+    document.addEventListener('keydown', onCloseEsc);
+    document.addEventListener('click', function onCloseClick(e) {
+        if (e.target === aboutModal) {
+            closeAboutModal();
+        }
+    });
 }
 
-function onKeyboardPush(e) {
+function onCloseEsc(e) {
     if (e.code === "Escape") {
-        closeModalTeam();
+        closeAboutModal();
     }
 }
 
-function cliskIntoVoid(e) {
-    if (e.target === modalTeam) {
-        closeModalTeam();
-    }
-}
-
-function closeModalTeam() {
-    modalTeam.classList.add('is-hidden');
-    bodyEl.classList.remove('modal-open');
-    document.removeEventListener('keydown', onKeyboardPush);
+function closeAboutModal() {
+    aboutModal.classList.add('is-hidden');
+    body.classList.remove('modal-open');
+    document.removeEventListener('keydown', onCloseEsc);
 }
 
 const swiper = new Swiper('.swiper', {
     modules: [Navigation, Pagination],
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+      },
     scrollbar: {
       el: '.swiper-scrollbar',
-    },
-    autoplay: {
-        delay: '2500',
-        disableOnInteraction: false,
+      draggable: true,
     },
     keyboard: {
         enabled: true,
         onlyInViewport: true,
         pageUpDown: true,
     },
-    speed: 500,
-    loop: true,
-    observer: true,
-    observeParents: true,
-    observeSlideChildren: true,
+    mousewheel: {
+        invert: true,
+        sensitivity: 1,
+        eventsTarget: "swiper"
+    },
+    centeredSlides: true,
     grabCursor: true,
     slideToClickedSlide: true,
+    loop: true,
     slidesPerView: 'auto',
-    slidesPerGroup: 1,
-    centeredSlides: true,
-    initialSlide: 0,
     freeMode: true,
-  });
 
-
-
-
-
-
-
-
-
-// import Swiper, { Navigation, Pagination } from 'swiper';
-
-// const btnOpenModalTeam = document.querySelector('.footer__btn');
-// const modalAboutUs = document.querySelector('.js-modal-about-us');
-// const body = document.querySelector('body');
-
-// btnOpenModalTeam.addEventListener('click', openModalTeam);
-
-// function openModalTeam() {
-//     modalAboutUs.classList.remove('is-hidden')
-//     body.classList.add('modal-open');
-
-//     document.addEventListener('keydown', onKeyboardPush);
-// }
-
-// function onKeyboardPush(e) {
-//     if (e.code === "Escape") {
-//         closeModal();
-//     }
-// }
-
-// function closeModal() {
-//     modalAboutUs.classList.add('is-hidden');
-//     body.classList.remove('modal-open');
-//     document.removeEventListener('keydown', onKeyboardPush);
-// }
-
-// const swiper = new Swiper('.swiper', {
-//     modules: [Navigation, Pagination],
-//     pagination: {
-//       el: '.swiper-pagination',
-//       clickable: true,
-//     },
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
-//     scrollbar: {
-//       el: '.swiper-scrollbar',
-//     },
-//     autoplay: {
-//         delay: '2500',
-//         disableOnInteraction: false,
-//     },
-//     keyboard: {
-//         enabled: true,
-//         onlyInViewport: true,
-//         pageUpDown: true,
-//     },
-//     speed: 500,
-//     loop: true,
-//     observer: true,
-//     observeParents: true,
-//     observeSlideChildren: true,
-//     grabCursor: true,
-//     slideToClickedSlide: true,
-//     slidesPerView: 'auto',
-//     slidesPerGroup: 1,
-//     centeredSlides: true,
-//     initialSlide: 0,
-//     freeMode: true,
-//   });
+});
