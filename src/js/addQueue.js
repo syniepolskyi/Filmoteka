1c88d8584a2c3fda953b209e4ff1bf3a5ac83b8e
Artem <ppt.artem@gmail.com>
2022-09-13 11:51:52 +0300

    update_from_main

--- src/js/addQueue.js
+++ src/js/addQueue.js
@@ -1,5 +1,11 @@
 import { dynRefs } from './constants/dynamicRefs';
 import { STORAGE, localStorageAPI } from './constants/storage';
+import { getMoviesDetails } from './api/moviedb/getMoviesDetails';
+import createMarkUp from '../templates/film-cards.hbs';
+import { refs } from './constants/refs';
+import { onFilmCardClick } from './onFilmCardClick';
+
+const { headerWatchedBtn, headerQueueBtn, mainList } = refs;
 
 const buttonLabelQueuedAdd = 'Add to queue';
 const buttonLabelQueuedRemove = 'Remove from queue';
@@ -7,7 +13,7 @@ const buttonLabelWatchedAdd = 'Add to watched';
 
 // Додає або видаляє фільм з localStorage в залежності від стану кнопки
 
-export function addQueue(e) {
+export async function addQueue(e) {
   const { addToWatchedBtn, addToQueueBtn } = dynRefs();
   const loadStorage = localStorageAPI.load(STORAGE);
   const { watched, queue } = loadStorage;
@@ -21,6 +27,13 @@ export function addQueue(e) {
 
     localStorageAPI.save(STORAGE, loadStorage);
 
+    if (
+      document.querySelector('.library__btns') &&
+      headerQueueBtn.classList.contains('button--accent')
+    ) {
+      removeFromLibraryList(id);
+    }
+
     e.target.setAttribute('data-btn', 'add');
     e.target.textContent = buttonLabelQueuedAdd;
     return;
@@ -28,14 +41,47 @@ export function addQueue(e) {
 
   queue.push(id);
 
+  if (
+    document.querySelector('.library__btns') &&
+    headerQueueBtn.classList.contains('button--accent')
+  ) {
+    try {
+      await addToLibrary(id);
+      document
+        .querySelector(`[data-action='${id}']`)
+        .addEventListener('click', onFilmCardClick);
+    } catch (error) {}
+  }
+
   if (indexOfWatchedMovie > -1) {
     watched.splice(indexOfWatchedMovie, 1);
     addToWatchedBtn.setAttribute('data-btn', 'add');
     addToWatchedBtn.textContent = buttonLabelWatchedAdd;
+
+    if (
+      document.querySelector('.library__btns') &&
+      !headerQueueBtn.classList.contains('button--accent')
+    ) {
+      removeFromLibraryList(id);
+    }
   }
 
   localStorageAPI.save(STORAGE, loadStorage);
 
   e.target.setAttribute('data-btn', 'remove');
   e.target.textContent = buttonLabelQueuedRemove;
+}
+function removeFromLibraryList(id) {
+  const activeFilm = document.querySelector(`[data-action='${id}']`);
+  activeFilm.remove();
+}
+async function addToLibrary(id) {
+  try {
+    let movieTopush = [];
+    const movie = await getMoviesDetails(id);
+    movieTopush.push(movie);
+    mainList.insertAdjacentHTML('afterbegin', createMarkUp(movieTopush));
+
+    return;
+  } catch (error) {}
 }
--- src/js/addWatched.js
+++ src/js/addWatched.js
@@ -1,5 +1,11 @@
 import { dynRefs } from './constants/dynamicRefs';
 import { STORAGE, localStorageAPI } from './constants/storage';
+import { getMoviesDetails } from './api/moviedb/getMoviesDetails';
+import createMarkUp from '../templates/film-cards.hbs';
+import { refs } from './constants/refs';
+import { onFilmCardClick } from './onFilmCardClick';
+
+const { headerWatchedBtn, headerQueueBtn, mainList } = refs;
 
 const buttonLabelWatchedAdd = 'Add to watched';
 const buttonLabelWatchedRemove = 'Remove from watched';
@@ -7,7 +13,7 @@ const buttonLabelQueuedAdd = 'Add to queue';
 
 // Додає або видаляє фільм з localStorage в залежності від стану кнопки
 
-export function addWatched(e) {
+export async function addWatched(e) {
   const { addToWatchedBtn, addToQueueBtn } = dynRefs();
   const loadStorage = localStorageAPI.load(STORAGE);
   const { watched, queue } = loadStorage;
@@ -20,6 +26,13 @@ export function addWatched(e) {
     watched.splice(indexOfWatchedMovie, 1);
 
     localStorageAPI.save(STORAGE, loadStorage);
+    // remove from library
+    if (
+      document.querySelector('.library__btns') &&
+      headerWatchedBtn.classList.contains('button--accent')
+    ) {
+      removeFromLibraryList(id);
+    }
 
     e.target.setAttribute('data-btn', 'add');
     e.target.textContent = buttonLabelWatchedAdd;
@@ -28,14 +41,47 @@ export function addWatched(e) {
 
   watched.push(id);
 
+  if (
+    document.querySelector('.library__btns') &&
+    headerWatchedBtn.classList.contains('button--accent')
+  ) {
+    try {
+      await addToLibrary(id);
+      document
+        .querySelector(`[data-action='${id}']`)
+        .addEventListener('click', onFilmCardClick);
+    } catch (error) {}
+  }
+
   if (indexOfQueuedMovie > -1) {
     queue.splice(indexOfQueuedMovie, 1);
     addToQueueBtn.setAttribute('data-btn', 'add');
     addToQueueBtn.textContent = buttonLabelQueuedAdd;
+
+    if (
+      document.querySelector('.library__btns') &&
+      !headerWatchedBtn.classList.contains('button--accent')
+    ) {
+      removeFromLibraryList(id);
+    }
   }
 
   localStorageAPI.save(STORAGE, loadStorage);
 
   e.target.setAttribute('data-btn', 'remove');
   e.target.textContent = buttonLabelWatchedRemove;
+}
+
+function removeFromLibraryList(id) {
+  const activeFilm = document.querySelector(`[data-action='${id}']`);
+  activeFilm.remove();
+}
+async function addToLibrary(id) {
+  try {
+    let movieTopush = [];
+    const movie = await getMoviesDetails(id);
+    movieTopush.push(movie);
+    mainList.insertAdjacentHTML('afterbegin', createMarkUp(movieTopush));
+    return;
+  } catch (error) {}
 }
